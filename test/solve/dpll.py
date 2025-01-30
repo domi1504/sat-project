7b87d87aba290fe80363a6b3c8aa5312b85f68eb
Dominik Pysch <domi.pysch@gmail.com>
2025-01-30 13:54:05 +0100

    tests

--- test/solve/dpll.py
+++ test/solve/dpll.py
@@ -5,13 +5,12 @@ from sat.solve.dpll.dpll import is_satisfiable_dpll
 from sat.solve.dpll.heuristics import jeroslaw_wang, jeroslaw_wang_two_sided, shortest_clause, dlis, dlcs, rdlcs, mom
 
 
-directory = "../samples/uf20_91"
-
-# Get all .cnf files in the directory
-cnf_files = [f for f in sorted(os.listdir(directory)) if f.endswith(".cnf")]
-
 heuristics = [dlis, dlcs, rdlcs, mom, jeroslaw_wang, jeroslaw_wang_two_sided, shortest_clause]
 
+# All satisfiable
+directory = "../samples/uf20_91"
+# Get all .cnf files in the directory
+cnf_files = [f for f in sorted(os.listdir(directory)) if f.endswith(".cnf")]
 
 @pytest.mark.parametrize("filename", cnf_files)
 @pytest.mark.parametrize("heuristic", heuristics)
@@ -33,3 +32,31 @@ def test_is_satisfiable_dpll_uf20_91(filename, heuristic):
         # Solve instance
         result = is_satisfiable_dpll(inst, heuristic)
         assert result == True
+
+
+# All unsatisfiable
+directory2 = "../samples/uuf50_218"
+# Get all .cnf files in the directory
+# Only first ten since this takes a while
+cnf_files2 = [f for f in sorted(os.listdir(directory2)) if f.endswith(".cnf")][:10]
+
+@pytest.mark.parametrize("filename", cnf_files2)
+@pytest.mark.parametrize("heuristic", heuristics)
+def test_is_satisfiable_dpll_uuf50_218(filename, heuristic):
+    """
+    Check for every instance of the uf50_218 whether False is returned.
+
+    :return:
+    """
+
+    file_path = os.path.join(directory2, filename)
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+        # Parse instance
+        inst = parse_dimacs_cnf(content)
+
+        # Solve instance
+        result = is_satisfiable_dpll(inst, heuristic)
+        assert result == False
