5e748daf7af4c1c5b02fc2286bf6c511b903a891
Dominik Pysch <domi.pysch@gmail.com>
2025-06-25 22:43:09 +0200

    migrate walksat tests

--- test/solve/test_gsat_with_walk.py
+++ test/solve/test_gsat_with_walk.py
@@ -1,85 +1,14 @@
-import os
 import pytest
-from sat.encoding.dimacs_cnf import parse_dimacs_cnf
 from sat.solve.local_search.greedy_sat_with_walk import is_satisfiable_gsat_with_walk
-
-# All satisfiable
-directory = "../samples/uf20_91"
-# Get all .cnf files in the directory
-cnf_files = [f for f in sorted(os.listdir(directory)) if f.endswith(".cnf")][:100]
+from test.solve.utils import get_satisfiable_instances, get_unsatisfiable_instances
 
 
-@pytest.mark.parametrize("filename", cnf_files)
-def test_is_satisfiable_gsat_with_walk_uf20_91(filename):
-    """
-    Check for every instance of the uf20_91 whether True is returned.
-
-    :return:
-    """
-
-    file_path = os.path.join(directory, filename)
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-
-        # Parse instance
-        inst = parse_dimacs_cnf(content)
-
-        # Solve instance
-        result = is_satisfiable_gsat_with_walk(inst)
-        assert result
+@pytest.mark.parametrize("instance", get_satisfiable_instances())
+def test_gsat_with_walk_sat(instance):
+    assert is_satisfiable_gsat_with_walk(instance)
 
 
-# All unsatisfiable
-directory2 = "../samples/small_unsat"
-# Get all .cnf files in the directory
-# Only first ten since this takes a while
-cnf_files2 = [f for f in sorted(os.listdir(directory2)) if f.endswith(".cnf")][:4]
+@pytest.mark.parametrize("instance", get_unsatisfiable_instances(only_small=True))
+def test_gsat_with_walk_unsat(instance):
+    assert not is_satisfiable_gsat_with_walk(instance)
 
-
-@pytest.mark.parametrize("filename", cnf_files2)
-def test_is_satisfiable_gsat_with_walk_small_unsat(filename):
-    """
-    Check for every instance of the uf50_218 whether False is returned.
-
-    :return:
-    """
-
-    file_path = os.path.join(directory2, filename)
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-
-        # Parse instance
-        inst = parse_dimacs_cnf(content)
-
-        # Solve instance
-        result = is_satisfiable_gsat_with_walk(inst)
-        assert not result
-
-
-# directory3 = "../samples/uuf50_218"
-# # Get all .cnf files in the directory
-# # Only first ten since this takes a while
-# cnf_files3 = [f for f in sorted(os.listdir(directory3)) if f.endswith(".cnf")][:10]
-#
-#
-# @pytest.mark.parametrize("filename", cnf_files3)
-# def test_is_satisfiable_schoening_uuf50_218(filename):
-#     """
-#     Check for every instance of the uf50_218 whether False is returned.
-#
-#     :return:
-#     """
-#
-#     file_path = os.path.join(directory3, filename)
-#
-#     with open(file_path, 'r', encoding='utf-8') as file:
-#         content = file.read()
-#
-#         # Parse instance
-#         inst = parse_dimacs_cnf(content)
-#
-#         # Solve instance
-#         result = is_satisfiable_schoening(inst, 1e-8)
-#         assert not result
